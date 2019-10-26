49aab115c7fde0d1d2c7b133775100596199c91a
olethomas93 <norbye93@gmail.com>
2019-10-26 15:47:16 +0200

    jk

--- ImageProcessing/ImageProcessing.py
+++ ImageProcessing/ImageProcessing.py
@@ -27,10 +27,10 @@ class imageProcessing(object):
             kSize = np.ones((35, 35), np.uint8)
 
             grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            brightGray = cv2.convertScaleAbs(grayImage, -1, alpha=5, beta=10)
+            grayImage = cv2.convertScaleAbs(grayImage, -1, alpha=5, beta=10)
             ret, bwImage = cv2.threshold(grayImage, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-            StructureElement = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
+            StructureElement = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (35, 35))
             erodedImage = cv2.erode(bwImage, StructureElement)
             dilatedImage = cv2.dilate(erodedImage, StructureElement)
             countors, _ = cv2.findContours(dilatedImage, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -52,7 +52,7 @@ class imageProcessing(object):
                 xCoor = width - xCoor
                 Ycoor = heigth - Ycoor
 
-                
+
 
                 # cor = Coordinate.Coordinate(xCoor, Ycoor)
 
