32c884307a98cb31f570d678cedbd7e494de3896
olethomas93 <norbye93@gmail.com>
2019-10-27 15:42:20 +0100

    Update test3.py

--- ImageProcessing/test3.py
+++ ImageProcessing/test3.py
@@ -32,7 +32,7 @@ class imageProcessing(object):
             #grayImage = cv2.convertScaleAbs(grayImage, alpha=2, beta=10)
             ret, bwImage = cv2.threshold(grayImage, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-            StructureElement = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+            StructureElement = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
 
             erodedImage = cv2.erode(bwImage, StructureElement)
             dilatedImage = cv2.dilate(erodedImage, StructureElement)
