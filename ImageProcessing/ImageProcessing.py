0c047511179154410a07e56d32ca2de1bdc0d6f6
olethomas93 <norbye93@gmail.com>
2019-10-27 16:10:37 +0100

    Update ImageProcessing.py

--- ImageProcessing/ImageProcessing.py
+++ ImageProcessing/ImageProcessing.py
@@ -15,6 +15,24 @@ class imageProcessing(object):
         processingQueue = list
 
     def processImage(self):
+        def nothing(x):
+            pass
+        cv2.namedWindow('temp')
+        cv2.createTrackbar('bl', 'temp', 0, 255, nothing)
+        cv2.createTrackbar('gl', 'temp', 0, 255, nothing)
+        cv2.createTrackbar('rl', 'temp', 0, 255, nothing)
+        cv2.createTrackbar('bh', 'temp', 255, 255, nothing)
+        cv2.createTrackbar('gh', 'temp', 255, 255, nothing)
+        cv2.createTrackbar('rh', 'temp', 255, 255, nothing)
+
+        bl_temp = cv2.getTrackbarPos('bl', 'temp')
+        gl_temp = cv2.getTrackbarPos('gl', 'temp')
+        rl_temp = cv2.getTrackbarPos('rl', 'temp')
+
+        bh_temp = cv2.getTrackbarPos('bh', 'temp')
+        gh_temp = cv2.getTrackbarPos('gh', 'temp')
+        rh_temp = cv2.getTrackbarPos('rh', 'temp')
+
 
         camera = PiCamera()
         camera.resolution = (640, 480)
@@ -23,6 +41,7 @@ class imageProcessing(object):
 
         for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
+            thresh = cv2.inRange(frame, (bl_temp, gl_temp, rl_temp), (bh_temp, gh_temp, rh_temp))
             image = frame.array
             kSize = np.ones((35, 35), np.uint8)
             kernel = np.ones((5, 5), np.float32) / 25
