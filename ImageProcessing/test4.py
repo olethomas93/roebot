d8778b994f932ea1268669b22f9e4b2c03303ba2
olethomas93 <norbye93@gmail.com>
2019-10-27 18:23:19 +0100

    Update test4.py

--- ImageProcessing/test4.py
+++ ImageProcessing/test4.py
@@ -63,7 +63,7 @@ class imageProcessing(object):
                 (x, y), r = cv2.minEnclosingCircle(c)
                 center = (int(x), int(y))
                 r = int(r)
-                if r >= 6 and r <= 10:
+                if r >= 8 and r <= 20:
                     cv2.circle(image, center, r, (0, 255, 0), 2)
                     array.append(center)
 
