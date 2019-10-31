16354be6e9f4af99475bc8eb721a3e2e5b4e72b6
olethomas93 <norbye93@gmail.com>
2019-10-31 13:34:20 +0100

    Update Camera.py

--- ImageProcessing/Camera.py
+++ ImageProcessing/Camera.py
@@ -27,6 +27,7 @@ class Camera:
 
         with picamera.PiCamera() as camera:
             with picamera.array.PiRGBArray(camera) as output:
+                camera.resolution=(640,480)
                 camera.capture(output, 'rgb')
                 print('Captured %dx%d image' % (
                     output.array.shape[1], output.array.shape[0]))
