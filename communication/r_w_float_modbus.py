a413953111f3eae3db06172cca59f79256c2424d
olethomas93 <norbye93@gmail.com>
2019-11-03 11:37:08 +0100

    Update r_w_float_modbus.py

--- communication/r_w_float_modbus.py
+++ communication/r_w_float_modbus.py
@@ -9,7 +9,7 @@ from pyModbusTCP import utils
 class FloatModbusClient():
 
     def __init__(self,Modbus):
-        self.ModbusClient = Modbus
+        self.ModbusClient = Modbus()
         self.reg_l = []
 
     def read_float(self, address, number=1):
