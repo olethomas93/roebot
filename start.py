041bcb5daa569eb9529d618217347b46c36937de
olethomas93 <norbye93@gmail.com>
2019-11-03 14:17:43 +0100

    Update start.py

--- start.py
+++ start.py
@@ -49,7 +49,7 @@ def processImages():
 
 def sendCordToPLC():
     client = r_w_float_modbus.FloatModbusClient(ModbusClient)
-    client.write_float(4,[4])
+    client.write_float(4,[float(4)])
 
 def switch_case(command):
     switcher = {
