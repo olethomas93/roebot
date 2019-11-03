edf6a0d40683c065b793dcd07064fe1cf142e233
olethomas93 <norbye93@gmail.com>
2019-11-03 11:42:41 +0100

    Update r_w_float_modbus.py

--- communication/r_w_float_modbus.py
+++ communication/r_w_float_modbus.py
@@ -3,17 +3,18 @@
 
 # how-to add float support to ModbusClient
 
+from pyModbusTCP.client import ModbusClient
 from pyModbusTCP import utils
 
 
-class FloatModbusClient():
+class FloatModbusClient(ModbusClient):
 
-    def __init__(self,Modbus):
-        self.ModbusClient = Modbus(host='158.38.140.52', port=2000, auto_open=True)
+    def __init__(self):
+        
         self.reg_l = []
 
     def read_float(self, address, number=1):
-        self.reg_l = self.ModbusClient.read_holding_registers(address, number * 1)
+        self.reg_l = self.read_holding_registers(address, number * 1)
         if self.reg_l:
             return self.reg_l, False
         else:
@@ -22,4 +23,4 @@ class FloatModbusClient():
     def write_float(self, address, floats_list):
         b32_l = [utils.encode_ieee(f) for f in floats_list]
         b16_l = utils.long_list_to_word(b32_l)
-        return self.ModbusClient.write_multiple_registers(address, b16_l)
+        return self.write_multiple_registers(address, b16_l)
