7cded8d909cd284517881196fbd051791dc4419a
olethomas93 <norbye93@gmail.com>
2019-11-05 18:16:45 +0100

    Update start.py

--- start.py
+++ start.py
@@ -35,7 +35,7 @@ def getDistanceFromTray():
 def takePicture():
     print("Executing take picture")
     camera = Camera.Camera()
-    RoeImage = camera.takePicture(800,62)
+    RoeImage = camera.takePicture(330,62)
     Roeimages.append(RoeImage)
 
 
