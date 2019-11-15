ea181623869f8274b48168c465c07b27c185aba4
olethomas93 <norbye93@gmail.com>
2019-11-15 15:07:14 +0100

    gj

--- RoebotMachine2.py
+++ RoebotMachine2.py
@@ -41,13 +41,14 @@ client = None
 # self.modbusclient = r_w_float_modbus.FloatModbusClient(ModbusClient)
 
 def poll_command():
+    global regList
     print("Polling server for commands")
     commandpoll = False
     # display loop (in main thread)
     while not commandpoll:
 
         # print regs list (with thread lock synchronization)
-
+        
         if regList:
             command = regList[0]
             if command in range(1, 6):
