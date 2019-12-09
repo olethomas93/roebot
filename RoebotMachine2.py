aabeb8a8aa3145bcd92a88df79b621daec8a05bc
olethomas93 <norbye93@gmail.com>
2019-12-09 20:36:46 +0100

    Update RoebotMachine2.py

--- RoebotMachine2.py
+++ RoebotMachine2.py
@@ -39,27 +39,26 @@ pictureIndex = 0
 camera = Camera.Camera()
 imageCv = imageprocessing3.imageProcessing()
 imageList = []
+client = None
 
 
 # self.modbusclient = r_w_float_modbus.FloatModbusClient(ModbusClient)
 
 def poll_command():
-    global client
+    global regList, regs_lock
     print("Polling server for commands")
-    client = ModbusClient(host=SERVER_HOST, port=SERVER_PORT)
     commandpoll = False
     # display loop (in main thread)
     while not commandpoll:
-        regList = client.read_holding_registers(0, 10)
-        if client.is_open():
-            print(list(regList)[0])
-        # print regs list (with thread lock synchronization)
-            if regList:
-                command = regList[0]
-                if command in range(1, 6):
 
-                    if sendIntModbus(0, 0):
-                        switch_case(command)
+        # print regs list (with thread lock synchronization)
+
+        if regList:
+            command = regList[0]
+            if command in range(1, 6):
+
+                if sendIntModbus(0, 0):
+                    switch_case(command)
 
 
 # Takes picture of tray.
@@ -314,22 +313,22 @@ if __name__ == '__main__':
 
     args = vars(ap.parse_args())
     # start a thread that will perform motion detection
-    th1 = Thread(target=detect_roe, args=(
-        32,))
+    #th1 = Thread(target=detect_roe, args=(
+        #32,))
 
     th2 = Thread(target=polling_thread)
     th3 = Thread(target=poll_command)
 
-    #th2.start()
-    th1.start()
+    #th1.start()
+    th2.start()
     th3.start()
 
-    app.run(host=args["ip"], port=8080, debug=True,
-            threaded=True, use_reloader=False)
+    #app.run(host=args["ip"], port=8080, debug=True,
+            #threaded=True, use_reloader=False)
 
 
 
 
     # start the flask app
 
-vs.stop()
+#vs.stop()
