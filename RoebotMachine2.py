4001d6474bf053abac68583ab564e4530ff99b98
olethomas93 <norbye93@gmail.com>
2019-12-09 19:57:14 +0100

    Update RoebotMachine2.py

--- RoebotMachine2.py
+++ RoebotMachine2.py
@@ -45,20 +45,20 @@ client = None
 # self.modbusclient = r_w_float_modbus.FloatModbusClient(ModbusClient)
 
 def poll_command():
-    global regList,regs_lock
+    global regList, regs_lock
     print("Polling server for commands")
     commandpoll = False
     # display loop (in main thread)
     while not commandpoll:
 
         # print regs list (with thread lock synchronization)
-        with regs_lock:
-            if regList:
-                command = regList[0]
-                if command in range(1, 6):
 
-                    if sendIntModbus(0, 0):
-                        switch_case(command)
+        if regList:
+            command = regList[0]
+            if command in range(1, 6):
+
+                if sendIntModbus(0, 0):
+                    switch_case(command)
 
 
 # Takes picture of tray.
