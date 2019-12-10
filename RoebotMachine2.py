df30a2e08db87e3c425d876b3961817950a17f61
olethomas93 <norbye93@gmail.com>
2019-12-10 12:42:54 +0100

    Update RoebotMachine2.py

--- RoebotMachine2.py
+++ RoebotMachine2.py
@@ -117,12 +117,13 @@ def processImages():
         print("processing images")
         imageList1, processing = imageCv.processImages()
         imageList = imageList1
-        time.sleep(1)
+
         if writecoilModbus(4, True):
+            time.sleep(1)
             switch_case(0)
     else:
         imageList = []
-
+        time.sleep(1)
         switch_case(0)
 
 
